47288414cc8c859f462a9c6bc831e52619441276
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2024-12-19 00:44:03 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,25 +4,25 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 require('dotenv').config();
 
-// Initialize Express app
+
 const app = express();
 
-// Middleware
+// Middlewares
 app.use(bodyParser.json());
 app.use(passport.initialize());
 
-// Database and Passport Configuration
+// Db y passport
 require('./config/database');
 require('./config/passport')(passport);
 
-// Routes
+// ruteo
 const userRoutes = require('./routes/userRoutes');
 const protectedRoutes = require('./routes/protectedRoutes');
 
 app.use('/api/users', userRoutes);
 app.use('/api/protected', protectedRoutes);
 
-// Server
+// puerto
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
